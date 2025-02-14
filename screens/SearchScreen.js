477ba763b240a46ea227792c3692f46fd301bafa
muhammed0nal <onalmuhammed11@gmail.com>
2025-02-14 17:33:04 +0300

    İlk commit

---
+++ screens/SearchScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function SearchSCREEN() {
+  return (
+    <View>
+      <Text>SearchSCREEN</Text>
+    </View>
+  );
+}
